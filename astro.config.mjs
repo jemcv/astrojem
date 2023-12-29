046d0908332700ea95d4c63674fbdf90a0047db2
jemcv <rangothechameleon@gmail.com>
2023-12-29 20:01:40 +0800

    Add remark-reading-time plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
+import { remarkReadingTime } from './remark-reading-time.mjs';
 
+import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  markdown: {
+    remarkPlugins: [remarkReadingTime()],
+  }
 });

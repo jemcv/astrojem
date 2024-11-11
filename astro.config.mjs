c6205586c9a652ea6b32a59064d9f0a7e593439e
jemcv <rangothechameleon@gmail.com>
2024-11-12 01:10:40 +0800

    Add node adapter support

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,16 +5,18 @@ import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vercel from '@astrojs/vercel/serverless';
+import node from '@astrojs/node';
 
-// https://astro.build/config
+// Determine the adapter based on command-line arguments
+const isNodeMode = process.argv.some(arg => arg.includes('--node'));
+const adapter = isNodeMode ? node({ mode: 'standalone' }) : vercel({ webAnalytics: { enabled: true } });
+
 export default defineConfig({
   output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
+  adapter: adapter,
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
   integrations: [react(), tailwind(), sitemap()],
-  site: "https:jemcv.me"
+  site: "https://jemcv.me"
 });
